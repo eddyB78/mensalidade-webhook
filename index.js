d27f81729a1b0f07ef8baad3e4fa57363316fb41
eddyB78 <73801896+eddyB78@users.noreply.github.com>
2026-04-20 02:23:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const admin = require("firebase-admin");
 const app = express();
 app.use(express.json());
 
-const serviceAccount = JSON.parse(fs.readFileSync("/home/ubuntu/firebase-key.json","utf8"));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || fs.readFileSync("/home/ubuntu/firebase-key.json","utf8"));
 admin.initializeApp({credential:admin.credential.cert(serviceAccount)});
 const db = admin.firestore();
 console.log("Firebase OK");
